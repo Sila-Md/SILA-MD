8d298d7e9e434a104d1a7230196e43f25269f9a7
Sila-Md <hackersila2@gmail.com>
2025-10-24 20:08:48 +0300

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -1,1 +1,14 @@
-R
+const config = require('../config');
+const { cmd } = require('../command');
+
+const MUSIC_URL = "https://files.catbox.moe/ebj284.jpg"; // Customize if needed
+
+cmd({
+    pattern: "repo",
+    alias: ["speed", "pong"],
+    use: '.repo',
+    desc: "Check bot's response time.",
+    category: "main",
+    react: "🍁",
+    filename: __filename
+},
