538b6e1682c65a85e74e987d368d2a9dedf63269
Sila-Md <hackersila2@gmail.com>
2025-10-24 19:22:14 +0300

    Update aivoice.js

--- plugins/aivoice.js
+++ plugins/aivoice.js
@@ -73,7 +73,7 @@ async (conn, mek, m, {
 
         // Send menu message with image
         const sentMsg = await conn.sendMessage(from, {  
-            image: { url: "https://files.catbox.moe/pworrs.jpg" },
+            image: { url: "https://files.catbox.moe/gnjb7s.jpg" },
             caption: menuText
         }, { quoted: m });
 
